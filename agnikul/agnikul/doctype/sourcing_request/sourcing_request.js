694db0e925bd53f05ec39a83efe935b8d4924461
vignesh <arumai.vigneshwaran@agnikul.in>
2021-12-28 10:15:11 +0530

    changes

--- agnikul/agnikul/doctype/sourcing_request/sourcing_request.js
+++ agnikul/agnikul/doctype/sourcing_request/sourcing_request.js
@@ -24,11 +24,90 @@ frappe.ui.form.on('Sourcing Request', {
 				alert("ASD");
 			}, __('Search'));
 			frm.add_custom_button(__('Internal Search Statement'), function() {
-				alert("ASD");
+				frappe.call({
+					method: "agnikul.agnikul.doctype.sourcing_request.sourcing_request.check_inventory_against_spec",
+					args: {
+						"spec": frm.doc.specification_sheet
+					},
+					callback: function (r) {
+						console.log(r.message);
+						var dialog = new frappe.ui.Dialog({
+							title: __("Internal Search Statement"),
+							fields: [
+								{fieldtype: "HTML", fieldname: "result", label: __("Result")},
+								]
+						});
+						var wrapper = dialog.fields_dict.result.$wrapper;
+						var html = `<table class="table">
+						<thead>
+						  <tr>
+							<th scope="col">Domain</th>
+							<th scope="col">Category</th>
+							<th scope="col">Key</th>
+							<th scope="col">Value</th>
+							<th scope="col">Must Have</th>
+							<th scope="col">Result</th>
+						  </tr>
+						</thead>
+						<tbody>`;
+						for (let i = 0; i < r.message.length; i++) {
+							const element = r.message[i];
+							let row = ``;
+							if (element.status == "Matching") {
+								row = `<tr style="background-color: lightgreen;">
+											<td>`+element.domain+`</td>
+											<td>`+element.category+`</td>
+											<td>`+element.key+`</td>
+											<td>`+element.value+`</td>
+											<td>`+element.must_have+`</td>
+											<td>`+element.status+`</td>
+										</tr>`
+							} else {
+								row = `<tr style="background-color: lightcoral;">
+											<td>`+element.domain+`</td>
+											<td>`+element.category+`</td>
+											<td>`+element.key+`</td>
+											<td>`+element.value+`</td>
+											<td>`+element.must_have+`</td>
+											<td>`+element.status+`</td>
+										</tr>`
+							}
+							
+							html = html + row
+						}
+						html = html + `</tbody>
+						</table>`
+						wrapper.html(html);
+						dialog.show();
+					}
+				})
 			}, __('Search'));
 			frm.add_custom_button(__('Market Search Statement'), function() {
 				window.open("https://www.google.com/search?q="+frm.doc.requested_item+" suppliers");
 			}, __('Search'));
 		}
+	}
+});
+
+frappe.ui.form.on("Sales Invoice Item", {
+	qty: function(frm, dt, dn) {
+		var d = locals[dt][dn];
+		if (d.qty > 0) {
+			d.amount = parseFloat(d.qty) * parseFloat(d.rate);
+		}
+		else{
+			d.qty = 0;
+		}
+		refresh_field("table_13");
+	},
+	rate: function(frm, dt, dn) {
+		var d = locals[dt][dn];
+		if (d.rate > 0) {
+			d.amount = parseFloat(d.qty) * parseFloat(d.rate);
+		}
+		else{
+			d.rate = 0;
+		}
+		refresh_field("table_13");
 	}
 });
--- agnikul/agnikul/doctype/sourcing_request/sourcing_request.py
+++ agnikul/agnikul/doctype/sourcing_request/sourcing_request.py
@@ -4,6 +4,7 @@
 from hashlib import new
 import frappe
 from frappe.model.document import Document
+from pypika import NULL
 
 class SourcingRequest(Document):
 	pass
@@ -13,7 +14,7 @@ def create_updated_spec(spec):
 	doc = frappe.get_doc("Specification Sheet", spec)
 	new_doc = frappe.new_doc("Specification Sheet")
 	new_doc.item = doc.item
-	new_doc.specification_version = float(doc.specification_version)+float(0.1)
+	new_doc.specification_version = "{:.2f}".format(float(doc.specification_version)+float(0.1))
 	new_doc.enhanced_specification = True
 	new_doc.base_specification = doc.name
 	new_doc.non_cryo = True if int(doc.non_cryo) == 1 else False 
@@ -24,3 +25,45 @@ def create_updated_spec(spec):
 		new_doc.append('table_9', i)
 	new_doc.insert()
 	return new_doc.name
+
+@frappe.whitelist()
+def check_inventory_against_spec(spec):
+	spec_sheet = frappe.get_doc("Specification Sheet", spec)
+	spec = spec_sheet.table_9
+	spec_data = []
+	for i in spec:
+		obj = {}
+		obj["domain"] = i.domain
+		obj["category"] = i.category
+		obj["key"] = i.key
+		obj["value"] = i.value
+		obj["must_have"] = i.must_have
+		spec_data.append(obj)
+
+	serial_nos_linked_with_item = frappe.db.get_list("Serial No",
+		fields = ["name"],
+		filters = {"item_code": spec_sheet.item}
+		)
+	
+	inventory_spec_data = []
+
+	for j in serial_nos_linked_with_item:
+		inventory_spec = frappe.get_doc("Serial No", j.name).specifications
+		for o in inventory_spec:
+			obj1 = {}
+			obj1["domain"] = o.domain
+			obj1["category"] = o.category
+			obj1["key"] = o.key
+			obj1["value"] = o.value
+			inventory_spec_data.append(obj1)
+
+	result = []
+	for u in spec_data:
+		for y in inventory_spec_data:
+			if u["key"] == y["key"] and u["domain"] == y["domain"] and u["category"] == y["category"] and u["value"] == y["value"]:
+				u["status"] = "Matching"
+				result.append(u)
+			else:
+				u["status"] = "Not Matching"
+				result.append(u)
+	return result

d0be18a5d8a954f91770433962e2b3dabba8b520
vignesh <arumai.vigneshwaran@agnikul.in>
2022-01-10 10:50:30 +0530

    changes

---
+++ agnikul/agnikul/doctype/agnikul_project_category/agnikul_project_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Vigneshwaran Arumainayagam and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Agnikul Project Category', {
+	// refresh: function(frm) {
+
+	// }
+});
